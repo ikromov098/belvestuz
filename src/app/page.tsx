dea33263242aef946a4c6fca621b04f614ba414a
Fotihjon <ikromov@usf.edu>
2026-07-19 18:26:03 +0500

    brand and content update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import CalculatorSection from '@/components/Calculator';
-import Testimonials from '@/components/Testimonials';
 import HeroSection from '@/components/HeroSection';
 import HowItWorks from '@/components/HowItWorks';
 import HalalSection from '@/components/HalalSection';
@@ -24,9 +23,6 @@ export default function Home() {
 
       {/* How it works */}
       <HowItWorks />
-
-      {/* Testimonials */}
-      <Testimonials />
 
       {/* Contact strip */}
       <ContactStrip />
